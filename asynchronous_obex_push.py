92627382cf30ff9e6e397cc297f36cad3aa6d12b
bookjan <johnsonsu@iis.sinica.edu.tw>
2016-01-22 08:52:27 +0000

    Bluez 4 & D-Bus API

--- asynchronous_obex_push.py
+++ asynchronous_obex_push.py
@@ -24,10 +24,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from bluetooth import *
 from PyOBEX.client import Client
 from threading import Thread
+from lxml import etree
 
 import select
 import time
 import itertools
+import dbus
+
+import logging
 
 
 class OBEX_Pusher(DeviceDiscoverer):
@@ -36,9 +40,12 @@ class OBEX_Pusher(DeviceDiscoverer):
         self.inquiry_time_start = time.time()
         self.done = False
         self.discovered_address = []
-        self.local_bdaddrs = ["00:1A:7D:DA:71:02",
-                              "00:1A:7D:DA:71:03", "00:1A:7D:DA:71:04"]
-        self.current_bdaddr = itertools.cycle(self.local_bdaddrs)
+        self.OBEX_UUID = '00001105-0000-1000-8000-00805f9b34fb'
+        self.CHANNEL_XPATH = etree.XPath(
+            "/record/attribute[@id='0x0004']/sequence/sequence/uuid[@value='0x0003']/../uint8/@value")
+        self.local_bdaddrs = {
+            "hci1": "00:1A:7D:DA:71:02", "hci0": "00:1A:7D:DA:71:03"}
+        self.current_hci = itertools.cycle(self.local_bdaddrs)
         self.signal_strength = -80
         print ("Inquiry Start!")
 
@@ -50,36 +57,69 @@ class OBEX_Pusher(DeviceDiscoverer):
             if address not in self.discovered_address:
                 self.discovered_address.append(address)
                 try:
-                    print ("   |- Created a Thread for OBJECT PUSH process")
-                    bdaddr = next(self.current_bdaddr)
+                    hci = next(self.current_hci)
+                    bus = dbus.SystemBus()
+                    manager = dbus.Interface(bus.get_object(
+                        "org.bluez", "/"), "org.bluez.Manager")
+                    adapter = dbus.Interface(bus.get_object("org.bluez", manager.FindAdapter(hci)),
+                                             "org.bluez.Adapter")
+                    # print ("   |- Created a Thread for OBJECT PUSH process")
                     Thread(target=self.object_push,
-                           args=(address, bdaddr,)).start()
+                           args=(address, hci, adapter,)).start()
                 except Exception as e:
                     print (e)
-        else:
-            print ("   |- Bluetooth Signal Strength is NOT GOOD!")
+        # else:
+        #     print ("   |- Bluetooth Signal Strength is NOT GOOD!")
 
-    def object_push(self, address, bdaddr):
-        s = BluetoothSocket(RFCOMM)
-        s.bind((bdaddr, 0))
+    def object_push(self, address, hci, adapter):
+        # s = BluetoothSocket(RFCOMM)
+        # bdaddr = self.local_bdaddrs[hci]
+        # s.bind((bdaddr, 0))
+
         start_time = time.time()
-        services = find_service(address=address, uuid=OBEX_OBJPUSH_CLASS)
+        channel = self.find_push_channel(address, adapter)
+        print ("[ %s ] OBEX OBJECT PUSH Channel is %s"% (address, channel))
         end_time = time.time()
-        print ("      |- FindingEndTime [ %s ] - Finding OBEX_OBJPUSH service from[ %s ]" % (
-            end_time - start_time, address))
-        if services:
-            channel = services[0]['port']
-            client = Client(address, channel)
-            print ("      |- OBEX_OBJPUSH Channel is %s" % channel)
-            try:
-                client.set_socket(s)
-                client.connect()
-                client.put('message.txt', 'Hello World!')
-                client.disconnect()
-            except IOError as e:
-                print (e)
+        # print ("      |- FindingEndTime [ %s ] - Finding OBEX_OBJPUSH service from[ %s ]" % (
+        #     end_time - start_time, address))
+
+        # if channel:
+        #     client = Client(address, channel)
+        #     print ("      |- OBEX_OBJPUSH Channel is %s" % channel)
+        #     try:
+        #         client.set_socket(s)
+        #         client.connect()
+        #         client.put('message.txt', 'Hello World!')
+        #         client.disconnect()
+        #     except IOError as e:
+        #         print (e)
+        # else:
+        #     print ("      |- NO OBEX_OBJPUSH PROFILE!!")
+
+    def find_push_channel(self, address, adapter=None, bus=dbus.SystemBus()):
+        try:
+            adapter.CreateDevice(address)
+            print("Device created: %s" % address)
+        except:
+            print("Device all ready known: %s" % address)
+
+        path = adapter.FindDevice(address)
+
+        device = dbus.Interface(bus.get_object("org.bluez", path),
+                                "org.bluez.Device")
+        properties = device.GetProperties()
+
+        if self.OBEX_UUID in properties['UUIDs']:
+            services = device.DiscoverServices(self.OBEX_UUID)
+            for key in services.keys():
+                root = etree.XML(str(services[key]))
+                res = self.CHANNEL_XPATH(root)
+                if len(res) > 0:
+                    return int(res[0], 16)
+            else:
+                return None
         else:
-            print ("      |- NO OBEX_OBJPUSH PROFILE!!")
+            return None
 
     def inquiry_complete(self):
         self.inquiry_time_end = time.time()
@@ -91,8 +131,11 @@ class OBEX_Pusher(DeviceDiscoverer):
         print ("  Discovered Bluetooth Address:")
         print (self.discovered_address)
 
-op = OBEX_Pusher(device_id=1)
-op.find_devices(lookup_names=True, duration=8, flush_cache=True)
+
+logging.basicConfig(level=logging.INFO)
+
+op = OBEX_Pusher(device_id=2)
+op.find_devices(lookup_names=False, duration=8, flush_cache=True)
 
 readfiles = [op, ]
 
